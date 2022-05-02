aa25fb2b6920b4ca5daae7c6779cb654a2ad4e96
Jmcdonald21 <jmcdonald21@horizon.csueastbay.edu>
2022-05-02 15:16:53 -0700

    Finished Stats Screen & adjusted some UI for flow (color scheme, etc.)

--- app/src/main/java/com/example/dogwalkingapp/StatScreen.java
+++ app/src/main/java/com/example/dogwalkingapp/StatScreen.java
@@ -1,13 +1,19 @@
 package com.example.dogwalkingapp;
 
 import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.BarChart;
@@ -23,6 +29,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -37,6 +44,9 @@ public class StatScreen extends AppCompatActivity {
     ArrayList<BarEntry> walksGraph = new ArrayList<>();
     ArrayList<Walks> useWalks = new ArrayList<>();
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    TextView startDate, endDate;
+    EditText eTextStart, eTextEnd;
+    DatePickerDialog picker;
 
     /**
      * Overrides the onCreate method of the StatScreen class
@@ -47,8 +57,53 @@ public class StatScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stat_screen);
+        eTextStart = (EditText) findViewById(R.id.textView);
+        eTextEnd = (EditText) findViewById(R.id.textView2);
+        eTextStart.setInputType(InputType.TYPE_NULL);
+        eTextEnd.setInputType(InputType.TYPE_NULL);
+        eTextStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(StatScreen.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                eTextStart.setText((monthOfYear + 1) + "/" + dayOfMonth + "/" + year);
+                                startTime.setDate(dayOfMonth);
+                                startTime.setMonth(monthOfYear);
+                                startTime.setYear(year - 1900);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
+        eTextEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(StatScreen.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                eTextEnd.setText((monthOfYear + 1) + "/" + dayOfMonth + "/" + year);
+                                endTime.setDate(dayOfMonth);
+                                endTime.setMonth(monthOfYear);
+                                endTime.setYear(year - 1900);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
     }
-
     /**
      * The loadGraph method creates the barchart that is used to display the information stored about the user's walk tracking
      * based on the user inputted date/times
@@ -58,14 +113,6 @@ public class StatScreen extends AppCompatActivity {
     public void loadGraph(View view) {
         BarChart barChart = findViewById(R.id.barChart);
         dao = new DAOWalks();
-        DatePicker startDatePicker = (DatePicker) findViewById(R.id.startDateSelector);
-        DatePicker endDatePicker = (DatePicker) findViewById(R.id.endDateSelector);
-        startTime.setDate(startDatePicker.getDayOfMonth());
-        startTime.setMonth(startDatePicker.getMonth());
-        startTime.setYear(startDatePicker.getYear() - 1900);
-        endTime.setDate(endDatePicker.getDayOfMonth());
-        endTime.setMonth(endDatePicker.getMonth());
-        endTime.setYear(endDatePicker.getYear() - 1900);
 
         readData(new MyCallBack() {
             @Override
@@ -79,7 +126,7 @@ public class StatScreen extends AppCompatActivity {
                     }
                 }
 
-                BarDataSet barDataSet = new BarDataSet(walksGraph, "Hours Walked");
+                BarDataSet barDataSet = new BarDataSet(walksGraph, "Miles Walked");
                 barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
                 barDataSet.setValueTextColor(Color.BLACK);
                 barDataSet.setValueTextSize(16f);
